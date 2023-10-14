6559b9252681850f0bbfdacc103790414e5eee50
khila-7 <akhilasiragam@gmail.com>
2023-10-15 00:41:31 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/khila-7'
+      className='link footer__link'
+    >
+      Created By Akhila
+    </a>
+  </footer>
+)
+
+export default Footer
